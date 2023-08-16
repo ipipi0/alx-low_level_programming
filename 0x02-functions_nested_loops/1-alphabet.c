953bccf9adfc67e87b3d3f0aaffd2c7e19294bcc
ipipi0 <ipipi.0023@gmail.com>
2023-08-15 19:16:20 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_alphabet - alphabet lowercase and new line
+ *
+ * Return: void
+ */
+
+void print_alphabet(void)
+{
+int letter;
+for (letter = 'a'; letter <= 'z'; letter++)
+_putchar (letter);
+_putchar ('\n');
+}
