d67602ef0a5209edd17b15ca1fa837770e235fb1
ipipi0 <ipipi.0023@gmail.com>
2023-08-21 10:03:11 -0700

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * reset_to_98 - Update values of the variables
+ * @n: to variable to update
+ * Return: 0 on success
+ */
+int main(void)
+{
+	int p = 98;
+	int *n;
+
+	n = &p;
+	return (0);
+}
+
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+This dir contain info for 0x05-pointers_arrays_strings projects
